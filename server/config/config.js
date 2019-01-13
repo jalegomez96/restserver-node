511db2e357d3d28564892b6f48dd85f023173867
jalegomez96 <jalegomez96@gmail.com>
2019-01-13 11:42:48 -0500

    Variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -8,12 +8,12 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 // BASE DE DATOS
 
-// let urlDB;
+let urlDB;
 
-// if (process.env.NODE_ENV === 'dev') {
-//     urlDB = 'mongodb://localhost:27017/cafe';
-// } else {
-urlDB = 'mongodb://cafe-user:abc123@ds155774.mlab.com:55774/cafe';
-// }
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = process.env.MONGO_URI;
+}
 
 process.env.URLDB = urlDB;
